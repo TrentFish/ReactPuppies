0b94e875077da390f4833c20a083172f891c025f
fishyboi@MSI <fishyboi@MSI>
2023-09-05 14:55:36 -0400

    Following along with the workshop made this easy

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,27 @@ import {puppyList} from './pupData.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+  
+  function handleClick() {}
+  
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
   return (<div className='App'>
     { 
    puppies.map((puppy) => {
-     return <p>{puppy.name}</p>
+     return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
    })
 }
+{ (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )} 
   </div>
   )
 }
